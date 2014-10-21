f47d72dc032cfc484c889b9691798742c6a528b0
Joe Grossberg <josephgrossberg+github@gmail.com>
2014-10-21 13:42:48 -0400

    WIP for brews on home laptop, after OS X Yosemite upgrade

---
+++ bin/rebrew.sh
@@ -0,0 +1,14 @@
+echo "brew prune";
+brew prune;
+echo "brew cleanup";
+brew cleanup;
+echo "brew doctor";
+brew doctor;
+echo "brew install imagemagick";
+brew install imagemagick;
+echo "brew install memcached";
+brew install memcached;
+echo "brew install most";
+brew install most;
+echo "brew install phantomjs";
+brew install phantomjs;
