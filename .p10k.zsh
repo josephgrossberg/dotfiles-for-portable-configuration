4e21ab9629ba5a5ce18ab365087b849eba1cb48e
Joe Grossberg <josephgrossberg+github@gmail.com>
2023-09-21 13:43:19 -0700

    brighter font color

--- .p10k.zsh
+++ .p10k.zsh
@@ -216,14 +216,14 @@
 
   ##################################[ dir: current directory ]##################################
   # Default current directory color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=239
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=242
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_middle
   # Replace removed segment suffixes with this symbol.
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER="⋯"
   # Color of the shortened directory segments.
-  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=242
+  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=245
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
   # segment is always an anchor.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=248
